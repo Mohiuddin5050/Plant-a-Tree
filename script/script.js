966a01b717f913f188040f82f13d54d148176b8b
Mohiuddin5050 <kmmohiuddin5050@gmail.com>
2025-09-09 03:25:54 +0600

    Calculate total price of trees in cart done

--- script/script.js
+++ script/script.js
@@ -1,3 +1,39 @@
+const cart = [];
+const addToCart = (name) => {
+    cart.push(name);
+    showCart();
+}
+const removeCart=(index)=>{
+    cart.splice(index, 1)
+    showCart()
+
+}
+
+const showCart = () => {
+    const cartItems = document.getElementById("cart-items")
+    const cartPrice = document.getElementById("cart-price")
+    cartItems.innerHTML = "";
+    let total = 0;
+
+    cart.forEach((item, index) => {
+        total += item.price;
+
+        const div = document.createElement("div")
+        div.innerHTML = `
+        <div class="flex justify-between items-center bg-[#F0FDF4] px-2  pb-2 rounded-lg mt-2">
+                         <div>
+                            <h2 class="font-semibold text-lg text-[#1f2937]">${item.name}</h2>
+                            <p class="text-gray-500">৳${item.price}</p>
+                        </div>
+                        <p onclick="removeCart(${index})" class="cursor-pointer">❌</p>
+                       </div>`
+
+        cartItems.append(div)
+
+    })
+    cartPrice.innerText=`Total: ৳${total}`
+}
+// <!-- <i class="fa-solid fa-xmark"></i> -->
 const allTrees = () => {
     fetch("https://openapi.programming-hero.com/api/plants")
         .then(res => res.json())
@@ -11,13 +47,13 @@ const showAllTrees = (trees) => {
         card.innerHTML = `
          <div class="bg-white p-3 rounded-xl space-y-3 flex flex-col h-full">
                         <img class="h-40 w-full rounded-xl" src="${tree.image}" alt="">
-                        <h2 onclick="loadDetail(${tree.id})" class="font-bold text-xl cursor-pointer inline-block">${tree.name}</h2>
+                        <h2 class="font-bold text-xl "><span onclick="loadDetail(${tree.id})" class="cursor-pointer ">${tree.name}</span></h2>
                         <p class="flex-grow">${tree.description}</p>
                         <div class="flex justify-between">
                             <button class="bg-[#DCFCE7] text-green-700 font-medium rounded-3xl px-3 py-1">${tree.category}</button>
-                            <h2 class="font-bold">$${tree.price}</h2>
+                            <h2 class="font-bold">৳${tree.price}</h2>
                         </div>
-                        <button class="btn btn-active btn-success w-full  rounded-3xl mt-auto">Success</button>
+                        <button onclick='addToCart({id:${tree.id}, name:"${tree.name}", price:${tree.price}})' class="btn btn-active btn-success w-full  rounded-3xl mt-auto text-white">Add to Cart</button>
                     </div>`
         treeContainer.append(card);
     })
@@ -48,13 +84,13 @@ const showPlants = (plants) => {
         card.innerHTML = `
          <div class="bg-white p-3 rounded-xl space-y-3 flex flex-col h-full">
                         <img class="h-40 w-full rounded-xl" src="${tree.image}" alt="">
-                        <h2 onclick="loadDetail(${tree.id})" class="font-bold text-xl cursor-pointer inline-block">${tree.name}</h2>
+                       <h2 class="font-bold text-xl "><span onclick="loadDetail(${tree.id})" class="cursor-pointer ">${tree.name}</span></h2>
                         <p class="flex-grow">${tree.description}</p>
                         <div class="flex justify-between">
                             <button class="bg-[#DCFCE7] text-green-700 font-medium rounded-3xl px-3 py-1">${tree.category}</button>
-                            <h2 class="font-bold">$${tree.price}</h2>
+                            <h2 class="font-bold">৳${tree.price}</h2>
                         </div>
-                        <button class="btn btn-active btn-success w-full  rounded-3xl mt-auto">Success</button>
+                        <button onclick='addToCart({id:${tree.id}, name:"${tree.name}", price:${tree.price}})' class="btn btn-active btn-success w-full rounded-3xl mt-auto text-white">Add to Cart</button>
                     </div>`
         plantsCard.append(card);
     })
@@ -71,9 +107,9 @@ const showDetail = (plants) => {
     <h2 class="font-bold text-2xl">${plants.name}</h2>
                     <img class="rounded-xl h-64 w-full" src="${plants.image}" alt="">
                     <p><span class="font-bold">Category: </span>${plants.category} </p>
-                    <h2><span class="font-bold">Price:</span> $${plants.price}</h2>
-                    <p><span class="font-bold">Description: </span>${plants.description}</p>
-                </div>`
+                    <h2><span class="font-bold">Price:</span> ৳${plants.price}</h2>
+                    <p><span class="font-bold">Description: </span>${plants.description}</p>`
+
     document.getElementById("my_modal_5").showModal();
 }
 
